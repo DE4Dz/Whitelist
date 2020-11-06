43d7a9b62d112ef56bbd462a5fdf8f6c210ac100
DE4Dz <60911776+DE4Dz@users.noreply.github.com>
2020-11-07 02:47:42 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 s = _G.Key or ""
-if game:HttpGet("https://WEBSITENAME.heroku.com/bot.js?key="..s) == "Whitelisted!" then
+if game:HttpGet("https://github.com/DE4Dz/Whitelist/blob/main/bot.js?key="..s) == "Whitelisted!" then
 print("Whitelisted")
-elseif game:HttpGet("https://WEBSITENAME.heroku.com/bot.js?key="..s) == "Not Whitelisted"
+elseif game:HttpGet("https://github.com/DE4Dz/Whitelist/blob/main/bot.js?key="..s) == "Not Whitelisted"
 game.Players.LocalPlayer:Kick("Not Whitelisted")
 else
 game.Players.LocalPlayer:Kick("Not Whitelisted")
